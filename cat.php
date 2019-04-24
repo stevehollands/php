37cbe9e010e07042f3e47fdc9763aae97ad538eb
stevehollands <46566028+stevehollands@users.noreply.github.com>
2019-04-24 14:13:11 +0200

    Add files via upload

---
+++ cat.php
@@ -0,0 +1,7 @@
+
+
+        <div>
+            <img src="Cat2.jpeg" alt="myPic" />
+        </div>
+        
+        <button><a href="index.php">link naar eerste pagina</a></button>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo "Hello!";
+?>
+
+    <div>
+    <img src="cat.jpeg" alt="myPic" />
+     <button><a href="cat.php">link naar tweede pagina</a></button>
+    </div>
+
+   
